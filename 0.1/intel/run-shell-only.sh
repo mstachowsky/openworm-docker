eb5a83c6269d9bf0b1e1c4e7e76d2cbfe509da4b
Stephen Larson <stephen.larson@gmail.com>
2017-11-23 13:26:11 -0500

    Adding shell only run script

---
+++ 0.1/intel/run-shell-only.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OW_OUT_DIR=/home/ow/shared
+
+xhost +
+
+docker run -ti \
+  --name openworm \
+  --device=/dev/dri:/dev/dri \
+  -e DISPLAY=$DISPLAY \
+  -e OW_OUT_DIR=$OW_OUT_DIR \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  --privileged \
+  -v $HOME/shared:$OW_OUT_DIR:rw \
+  openworm-intel:0.1 \
+  bash
